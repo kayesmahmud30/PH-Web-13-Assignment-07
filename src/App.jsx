0ebd76049ed2ec0767825d3222ab9305070444a1
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-04-15 05:14:57 +0600

    Modified the toaster's position

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { RouterProvider } from "react-router"
-import { router } from "./routes/AppRouter"
-import ActionPrvider from "./context/actioncontext/ActionProvider"
-import { ToastContainer } from "react-toastify"
-
-
+import { RouterProvider } from "react-router";
+import { router } from "./routes/AppRouter";
+import ActionPrvider from "./context/actioncontext/ActionProvider";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-
     <ActionPrvider>
       <RouterProvider router={router} />
 
-      <ToastContainer theme="dark" closeOnClick pauseOnHover autoClose={1500} position="top-center" className='rounded-full p-0' />
-    </ActionPrvider >
-
-
-
-  )
+      <ToastContainer />
+    </ActionPrvider>
+  );
 }
 
-export default App
+export default App;
